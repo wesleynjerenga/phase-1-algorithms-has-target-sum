659c7b3881719c1d6087f99fafbe68843cacf215
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-04-01 09:31:45 +0300

    finished

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+    seenNumbers.add(number);
+  }
+
+  return false;
 }
 
 /* 
